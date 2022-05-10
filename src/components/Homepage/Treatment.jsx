e29cf96942759ed6aa2030c0536a01b0ad1a2017
Ar1f007 <muhammadarif9416@yahoo.com>
2022-05-11 00:05:39 +0600

    treatment sec ui

---
+++ src/components/Homepage/Treatment.jsx
@@ -0,0 +1,29 @@
+import treatment from '../../assets/images/treatment.png';
+import { Button } from '../Shared/Button';
+
+export const Treatment = () => {
+  return (
+    <div className="hero bg-base-100 pb-20 lg:pb-28">
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          src={treatment}
+          className="w-[90%] max-w-md rounded-lg shadow-2xl mb-4 lg:mb-0"
+          alt="A boy is getting treatment"
+        />
+        <div className="lg:pl-12">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-neutral">
+            Exceptional Dental Care, on Your Terms
+          </h1>
+          <p className="py-6 lg:pr-20">
+            It is a long established fact that a reader will be distracted by the readable content
+            of a page when looking at its layout. The point of using Lorem Ipsumis that it has a
+            more-or-less normal distribution of letters,as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing packages and web
+            page
+          </p>
+          <Button text="get started" link="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
