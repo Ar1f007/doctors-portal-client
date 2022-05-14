5b8ef247fd51e33a8b526bf6bdc82529edcca339
Ar1f007 <muhammadarif9416@yahoo.com>
2022-05-14 19:41:38 +0600

    dashboard ui style top part done

--- src/components/DashboardPage/Drawer.jsx
+++ src/components/DashboardPage/Drawer.jsx
@@ -1,26 +1,70 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
 export const Drawer = () => {
+  const location = useLocation();
+
+  const matchRoute = (route) => {
+    if (route === location.pathname) {
+      return true;
+    }
+  };
+
   return (
-    <div className="drawer drawer-mobile">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
-        {/* <!-- Page content here --> */}
-        <h1 className="text-2xl lg:text-4xl text-secondary font-medium text-center">Dashboard</h1>
+    <div className="drawer drawer-mobile mt-3 lg:border border-base-200 ">
+      <input id="sidebar" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content py-[.92rem] border-t-[1px] lg:border-t-0">
+        <div className="navbar lg:bg-transparent">
+          <div className="navbar-start w-max">
+            <label htmlFor="sidebar" tabIndex="1" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+          </div>
+          <div className="flex-1 justify-center navbar-center">
+            <h1 className="uppercase text-base lg:text-xl font-bold text-primary">
+              Welcome to your Dashboard
+            </h1>
+          </div>
+        </div>
         <Outlet />
-        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
-          Open Sidebar
-        </label>
+
+        {/* <!-- Page content here --> */}
       </div>
       <div className="drawer-side">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        <label htmlFor="sidebar" className="drawer-overlay"></label>
+        <ul className="lg:bg-gray-50 menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
           {/* <!-- Sidebar content here --> */}
           <li>
-            <Link to="/dashboard">My Appointments</Link>
+            <Link
+              to="/dashboard"
+              className={
+                matchRoute('/dashboard') ? 'bg-primary' : 'bg-transparent hover:bg-base-200'
+              }
+            >
+              My Appointments
+            </Link>
           </li>
           <li>
-            <Link to="/dashboard/review">Review</Link>
+            <Link
+              to="/dashboard/review"
+              className={
+                matchRoute('/dashboard/review') ? 'bg-primary' : 'bg-transparent hover:bg-base-200'
+              }
+            >
+              Review
+            </Link>
           </li>
         </ul>
       </div>
--- src/components/DashboardPage/MyAppointments.jsx
+++ src/components/DashboardPage/MyAppointments.jsx
@@ -1,3 +1,9 @@
 export const MyAppointments = () => {
-  return <div>MyAppointments</div>;
+  return (
+    <main>
+      <h2 className="capitalize text-xl lg:text-2xl font-normal text-neutral lg:-mt-5 text-center font-medium">
+        My Appointments
+      </h2>
+    </main>
+  );
 };
